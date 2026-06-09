726867b8fc909e3fe0f2cb9f1ea15029860d265a
kienle141204 <letrungkien14122004@gmail.com>
2026-06-09 11:14:20 +0700

    refactor

---
+++ code/scripts/pems08.sh
@@ -0,0 +1,38 @@
+python main.py \
+    --data_path '../../data/traffic/PEMS08/PEMS08.npz' \
+    --adj_filename ../../data/traffic/PEMS08/PEMS08.csv \
+    --dataset PEMS08FLOW \
+    --desc test_pems08 \
+    --sample_len 12 \
+    --predict_len 12 \
+    --train_ratio 0.6 \
+    --val_ratio 0.2 \
+    --epoch 5 \
+    --val_epoch 1 \
+    --test_epoch 5 \
+    --batch_size 32 \
+    --lr 0.001 \
+    --causal 0 \
+    --model gpt2 \
+    --lora \
+    --ln_grad \
+    --patience 50 \
+    --t_dim 64 \
+    --node_emb_dim 64 \
+    --node_embedding \
+    --llm_layers 3 \
+    --time_token \
+    --dropout 0.05 \
+    --trunc_k 64 \
+    --weight_decay 0 \
+    --task prediction \
+    --sandglassAttn 1 \
+    --sag_dim 128 \
+    --sag_tokens 128 \
+    --input_dim 1 \
+    --output_dim 1 \
+    --anchor_day_loss_weight 0.1 \
+    --anchor_week_loss_weight 0.1 \
+    --anchor_loss_type mae \
+    --use_anchor_day \
+    --use_anchor_week
